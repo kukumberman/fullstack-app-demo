448b1dc30a3612dfcace927b0e4506e74017d254
Dima <strongbaby1337@gmail.com>
2023-03-09 19:18:15 +0200

    Add - jwt test todo

---
+++ Server/test/jwt.test.ts
@@ -0,0 +1,13 @@
+describe("/api/refresh", () => {
+  it.todo("successfully updates refreshToken and returns new token pair")
+
+  it.todo("fails to generate new token pair if given refreshToken dont exist")
+
+  it.todo("user can use refreshToken only once")
+})
+
+describe("logout", () => {
+  it.todo("user can logout and it removes current token")
+
+  it.todo("user can logout all sessions")
+})
