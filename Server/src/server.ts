290fa664ea34e40dd6d84d02f0e9842e7401e56d
Dima <strongbaby1337@gmail.com>
2023-03-12 22:02:28 +0200

    Add - fastify swagger defaults

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -38,6 +38,20 @@ export async function createServer(app: Application): Promise<FastifyInstance> {
     }
   )
 
+  await fastify.register(import("@fastify/swagger"), {
+    swagger: {
+      info: {
+        title: "Test swagger",
+        description: "Testing the Fastify swagger API",
+        version: "0.1.0",
+      },
+    },
+  })
+
+  await fastify.register(import("@fastify/swagger-ui"), {
+    routePrefix: "/swagger",
+  })
+
   fastify.get("/ping", (request, reply) => {
     return { message: "pong" }
   })
